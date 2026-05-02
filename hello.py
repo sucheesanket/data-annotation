53bf30ba88e46aa62d0c7d189f26b55dac7eb68d
sucheesanket <167225590+sucheesanket@users.noreply.github.com>
2026-05-02 22:07:21 +0530

    small-change

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello World")
 name= input("Enter your name:")
 print(name)
+print(name)
---
+++ string.py
@@ -0,0 +1,6 @@
+my_string="hey it's sanket"
+count=0
+for i in my_string:
+    count=count+1
+    
+    print("length of the string is :",my_string)
