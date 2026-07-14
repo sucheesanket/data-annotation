2a896c2c8dc8b79a9115b610d4a72bd9c8101bc3
sucheesanket <167225590+sucheesanket@users.noreply.github.com>
2026-07-14 23:41:42 +0530

    fundamentals

---
+++ for exam/start.py
@@ -0,0 +1,33 @@
+# age=21
+# # name="sanket"
+# print(age)
+# x=10
+# x=30
+# print(x)
+# isstudent=True
+# print(isstudent)
+# name=input("Enter your name:")
+# print(name)
+# age=int(input("Enter your age: "))
+# print(age)
+# x=30
+# y=x+age
+# print(y)
+# for i in range(2,15):
+#     print(i)
+# count=3
+# while count<=5:
+#     print(count)
+#     count+=1
+# def sanket(name):
+#     print("Teri maa ki chuutt",name)
+#     print("Teri maa kaa bhosda",name)
+
+    
+# sanket("linkan")    
+
+# file=open("start.py","r")
+# print(file)
+with open ("start.py","r") as file:
+    data = file.read()
+    print(data)
